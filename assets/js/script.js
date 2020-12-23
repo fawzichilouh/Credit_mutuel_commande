186f2e4f251b2c10758cdb32229dbe502fb45bc5
fawzichilouh <61601372+fawzichilouh@users.noreply.github.com>
2020-12-24 00:20:55 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
-$(document).ready(function() {
-    $(".btn1").click(function() {
-        $(".btn1").addClass("d-none")
-        $(".inf-1").addClass("d-none")
-        $(".access").addClass("d-none")
-        $(".inf-2").removeClass("d-none")
-    });
+$(document).ready(function () {
+  $(".btn1").click(function () {
+    $(".btn1").addClass("d-none");
+    $(".inf-1").addClass("d-none");
+    $(".access").addClass("d-none");
+    $(".inf-2").removeClass("d-none");
+    var username = $("#username").val();
+    sessionStorage.setItem("lastname", username);
+    $("#usernametape2").val(sessionStorage.getItem("lastname"));
+  });
+  $("#icon_back").click(function () {
+    $(".btn1").removeClass("d-none");
+    $(".inf-1").removeClass("d-none");
+    $(".access").removeClass("d-none");
+    $(".inf-2").addClass("d-none");
+    var username = $("#username").val();
+    sessionStorage.setItem("lastname", username);
+    $("#usernametape2").val(sessionStorage.getItem("lastname"));
+  });
 
-
-    $(".toggle-password").click(function() {
-
-        $(this).toggleClass("fa-eye fa-eye-slash");
-        var input = $($(this).attr("toggle"));
-        if (input.attr("type") == "password") {
-            input.attr("type", "text");
-        } else {
-            input.attr("type", "password");
-        }
-    });
-
+  $(".toggle-password").click(function () {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
 });
